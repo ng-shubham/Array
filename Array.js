c6e91938e3b92267148256feb4e268d6087c1fa7
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-12 23:51:24 -0700

    Add files via upload
    Array declaration and accessing the array elements.

---
+++ Array.js
@@ -0,0 +1,10 @@
+//Declaration of Array
+let fruits = ['Apple', 'Orange', 'Chiku', 'Banana'];
+console.log(fruits);
+
+//Access the Array element using index
+console.log(fruits[1]);
+
+//Access the last element of an Array
+let last = fruits[fruits.length-1]
+console.log(last);
