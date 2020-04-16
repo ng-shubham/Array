2ff61806acebc6b87f31e88b432abfa8c36fc444
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-16 02:27:06 -0700

    Create fill.js

---
+++ fill.js
@@ -0,0 +1,4 @@
+ const array = [4, 8, 4, 87, 10, 22, 6]
+
+ console.log(array);                            //[4, 8, 4, 87, 10, 22, 6]
+ console.log(array.fill(5, 2, 6));              //[4, 8, 5, 5, 5, 5, 6]
