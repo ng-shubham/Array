5327a7b609562d32a5b0071cb1a16274b06ab421
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-17 06:01:12 -0700

    Create indexOf.js
    indexOf() method in an Array.

---
+++ indexOf.js
@@ -0,0 +1,6 @@
+
+const array = [2, 40, 15, 10, 35, 54, 40];
+
+console.log(array.indexOf(40));          //1
+console.log(array.indexOf(40, 3));       //6
+console.log(array.indexOf(30));          //-1          
