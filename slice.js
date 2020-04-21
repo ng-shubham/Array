6056738f785b01b240499b03178baab4582192b6
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-21 07:15:20 -0700

    Create slice.js
    slice() method in an Array.

---
+++ slice.js
@@ -0,0 +1,6 @@
+const number = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+console.log(number.slice(2, 4));            //["Three", "Four"]
+console.log(number.slice(4));               //["Five", "Six"]
+console.log(number.slice(1, 5));            //["Two", "Three", "Four", "Five"]
+
+
