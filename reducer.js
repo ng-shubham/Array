5221d9c2573e76f803975c6ae5bdbde2cdaa3f4c
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-21 04:20:02 -0700

    Delete reducer.js

--- reducer.js
+++
@@ -1,7 +0,0 @@
-const fun = (currentValue, addedValue) => addedValue + currentValue; 
-const array = [2, 5, 4, 3]; 
-let reducer = array.reduce(fun);
-console.log(reducer);                                       //14
-
-let reducerValue = array.reduce(fun, 10);
-console.log(reducerValue);                                  //24
