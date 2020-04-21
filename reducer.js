81d5fc85e7472bb6256c2dc541ace70472e00bd6
Shubham Ambhore <53085156+ng-shubham@users.noreply.github.com>
2020-04-21 03:52:10 -0700

    Update reducer.js
    reduce() method in an Array.

--- reducer.js
+++ reducer.js
@@ -3,6 +3,5 @@ const array = [2, 5, 4, 3];
 let reducer = array.reduce(fun);
 console.log(reducer);                                       //14
 
-
 let reducerValue = array.reduce(fun, 10);
 console.log(reducerValue);                                  //24
